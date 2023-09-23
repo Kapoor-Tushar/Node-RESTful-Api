35fcc3a8ec0ccc5b519e9afec835cecad34f2071
Tushar Kapoor <82567111+Kapoor-Tushar@users.noreply.github.com>
2023-09-23 15:31:18 +0530

    Add files via upload

---
+++ Node-Rest-shop/api/models/products.js
@@ -0,0 +1,19 @@
+// Importing mongoose package to help us make the schema
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+	_id: mongoose.Schema.Types.ObjectId,
+	name: {
+		type: String,
+		required: true,
+	},
+	price: {
+		type: Number,
+		required: true,
+	},
+	productImage: {
+		type: String,
+		// required:true,
+	},
+});
+module.exports = mongoose.model("Product", productSchema);
